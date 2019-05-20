9f3eec88b94b36492b218d5731ff8cc7a2dcba70
Marko <35701519+Brika28@users.noreply.github.com>
2019-05-20 17:42:00 +0200

    Add files via upload

--- mojaTvrtka.php
+++ mojaTvrtka.php
@@ -102,8 +102,7 @@ else if($_SESSION['activeUserType']==1)
 				echo "</tbody>";
 				echo "</table>";
 		}
-	$employeesList="SELECT korisnik.ime,korisnik.prezime,korisnik.korisnik_id,zaposlenik.zaposlenik_id,zaposlenik.tvrtka_id,tvrtka.tvrtka_id,
-						tvrtka.moderator_id
+	$employeesList="SELECT korisnik.ime,korisnik.prezime,korisnik.korisnik_id
 						FROM zaposlenik
 						INNER JOIN korisnik ON korisnik.korisnik_id = zaposlenik.korisnik_id
 						INNER JOIN tvrtka ON tvrtka.tvrtka_id=zaposlenik.tvrtka_id
@@ -129,7 +128,7 @@ else if($_SESSION['activeUserType']==1)
 					echo "</tbody>";
 					echo "</table>";
 		}
-	$freeEmployees ="SELECT korisnik.ime,korisnik.prezime,korisnik.korisnik_id,tvrtka.moderator_id,zaposlenik.zaposlenik_id,zaposlenik.korisnik_id
+	$freeEmployees ="SELECT korisnik.ime,korisnik.prezime,korisnik.korisnik_id
 					FROM korisnik
 					LEFT OUTER JOIN zaposlenik ON zaposlenik.korisnik_id=korisnik.korisnik_id
 					LEFT OUTER JOIN tvrtka ON korisnik.korisnik_id = tvrtka.moderator_id
@@ -171,7 +170,11 @@ else if($_SESSION['activeUserType']==1)
 			list($zahtjev)=mysqli_fetch_array($resultStatus);
 			$_SESSION['zahtjev']=$zahtjev;
 		}
-	}
+
+			echo "<h2> Zahtjev za dodatnim odgovorima </h2>";
+			echo "<form action ='mojaTvrtka.php' method = 'POST'> ";
+			echo "<input type = 'submit' name = 'posaljiZahtjev' value='Pošalji zahtjev za odgovorima'> ";
+			echo "</form>";
 
 			if(isset($_POST['posaljiZahtjev']))
 			{
@@ -181,7 +184,8 @@ else if($_SESSION['activeUserType']==1)
 								set zahtjev = 1
 								WHERE tvrtka.tvrtka_id =".$_SESSION ['tvrtkaId'];
 
-				$resultUpdate = queryDB ($connect,$updateZahtjev);		
+				$resultUpdate = queryDB ($connect,$updateZahtjev);
+				echo "Zahtjev uspiješno prosljeđen!";		
 				}
 				else
 				{
@@ -189,15 +193,121 @@ else if($_SESSION['activeUserType']==1)
 				}
 				
 			}
-					
-			
-				echo "<form action ='mojaTvrtka.php' method = 'POST'> ";
-				echo "<input type = 'submit' name = 'posaljiZahtjev' value='Šalji'> ";
+	}
+		echo "<h2> Statistika po zaposleniku </h2>";
 
+		$getTime ="SELECT datum_vrijeme_pitanja 
+							FROM pitanje 
+							LEFT JOIN tvrtka ON tvrtka.tvrtka_id = pitanje.tvrtka_id 
+							WHERE tvrtka.moderator_id =".$_SESSION['activeUserId'];
+			$resultTime = queryDB($connect,$getTime);
+		
+
+?>
 			
-	
-disconnectDB($connect);
+<form action="mojaTvrtka.php" method="POST">
+
+
+	<select name="dropdown">
+<?php	
+	if($resultTime)
+		{
+
+			while($row = mysqli_fetch_array($resultTime))
+    		{  ?>    
+				<option value="<?php echo $row['datum_vrijeme_pitanja'];?>">
+					<?php echo $row['datum_vrijeme_pitanja']; ?>
+				</option>
+			<?php } ?>
+		}
+?>
+	</select>
+<?php
+			echo "DO";
+
+			$getTime1 ="SELECT datum_vrijeme_pitanja 
+							FROM pitanje 
+							LEFT JOIN tvrtka ON tvrtka.tvrtka_id = pitanje.tvrtka_id 
+							WHERE tvrtka.moderator_id =".$_SESSION['activeUserId'];
+			$resultTime1 = queryDB($connect,$getTime1);
+?>
+		
+<select name="dropdown1">
+<?php	
+	if($resultTime1)
+		{
+
+			while($row = mysqli_fetch_array($resultTime1))
+    		{  ?>    
+				<option value="<?php echo $row['datum_vrijeme_pitanja'];?>">
+					<?php echo $row['datum_vrijeme_pitanja']; ?>
+				</option>
+			<?php } ?>
+		}
+?>
+</select>
+ <?php
+    if(isset($_POST['dropdown']))
+    {
+    	$selectedDatum = $_POST['dropdown'];
+    }
+        if(isset($_POST['dropdown1']))
+    {
+    	$selectedDatum1 = $_POST['dropdown1'];
+    }
+    echo
+      "<form action='' method='post'>
+        <input type='submit' name='zatraziRezultat' value='Zatrazi rezultat!' />
+       </form>";
+
+		if(isset($_POST['zatraziRezultat']))
+		{
+			$statsQuery ="SELECT ime, prezime, COUNT(*) AS broj_odgovora FROM korisnik, zaposlenik, odgovor
+								WHERE korisnik.korisnik_id = zaposlenik.korisnik_id AND zaposlenik.zaposlenik_id = odgovor.zaposlenik_id
+								AND zaposlenik.tvrtka_id ='{$_SESSION['tvrtkaId']}' AND odgovor.datum_vrijeme_odgovora BETWEEN 
+								'{$selectedDatum}' AND 
+								'{$selectedDatum1}'
+								 GROUP BY korisnicko_ime";
+								
+				$statsResult = queryDB($connect,$statsQuery);
+
+				if(mysqli_num_rows($statsResult) > 0)
+				{
+
+			echo "<h2> Popis odgovora </h2>";
+			echo "<table border ='1'>";
+			echo "<thead>";
+			echo "<tr>";
+			echo "<th>Ime i prezime </th>";
+			echo "<th>Odgovori </th>";
+			echo "</tr>";
+			echo "</thead>";
+			echo "<tbody>";
+
+					while(list($ime,$prezime,$broj_odgovora)=mysqli_fetch_row($statsResult))
+					{
+						echo "<tr>";
+						echo "<td>".$ime." ".$prezime."</a></td>";
+						echo "<td>".$broj_odgovora."</td>";
+						echo "</tr>";
+					}
+					echo "</tbody>";
+					echo "</table>";
+
+
+					}
+					else 
+					{
+						echo"Nema rezultata za traženo razdoblje!";
+					}
+			}
+		}
+	}
 }
+
+										
+disconnectDB($connect);
+
 ?>
 </body>
 </html>
